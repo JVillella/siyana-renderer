697e04a24ab696af30603b09730d909391f00f62
Julian Villella <julian.villella@hotmail.com>
2016-02-21 00:26:05 -0500

    Add version file

---
+++ siyana-renderer/src/Version.h
@@ -0,0 +1,18 @@
+//
+//  Version.h
+//  siyana-renderer
+//
+//  Created by Julian Villella on 2016-02-20.
+//  Copyright © 2016 Julian Villella. All rights reserved.
+//
+
+#ifndef Version_h
+#define Version_h
+
+using namespace std;
+
+namespace SiyanaRenderer {
+    string kBuildVersion = "0.5";
+}
+
+#endif /* Version_h */
